830705c7e2c0d11a9a05ad70623b21eef8ba3d34
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2025-11-11 00:14:21 +0200

    Commit 11.11.2025

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,64 @@
-import React from 'react'
-import './Navbar.scss'
+import React, { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+import "./Navbar.scss";
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-        <div className='container'>
-            <div className='logo'>
-                <span className='text'>fiverr</span>
-                <span className='dot'>.</span>
-            </div>
-            <div className='links'>
-                <span>Fiverr Business</span>
-                <span>Explore</span>
-                <span>English</span>
-                <span>Sign in</span>
-                <span>Become a Seller</span>
-                <button>Join</button>
-            </div>
-        </div>
-    </div>
-  )
-}
+  const [active, setActive] = useState(false);
 
-export default Navbar
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  const currentUser = {
+    id: 1,
+    username: "John Doe",
+    isSeller: true,
+  };
+
+  return (
+    <div className={active ? "navbar active" : "navbar"}>
+      <div className="container">
+        <div className="logo">
+          {/* <Link to="/">  */}
+          <span className="text">fiverr</span>
+          {/* </Link> */}
+          <span className="dot">.</span>
+        </div>
+        <div className="links">
+          <span>Fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign in</span>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          {!currentUser && <button>Join</button>}
+          {currentUser && (
+            <div className="user">
+              <img src="" alt="" />
+              <span>{currentUser?.username}</span>
+              <div className="options"></div>
+            </div>
+          )}
+        </div>
+      </div>
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>Test</span>
+            <span>Test2</span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
